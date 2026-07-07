1afd17a6bd4765a81390cf489bedca36e3068ed6
gaeunpark7 <hen77360@gmail.com>
2026-07-07 22:20:50 +0900

    chore: 앱 패키지명 변경

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:free_log/core/theme/app_colors.dart';
+import 'package:free_log/core/theme/app_text_style.dart';
 import 'package:free_log/di/auth_provider_setup.dart';
 import 'package:free_log/features/auth/presentation/providers/auth_provider.dart';
 import 'package:free_log/features/auth/presentation/widgets/login_button_widget.dart';
+import 'package:free_log/l10n/app_localizations.dart';
 
 class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
@@ -29,25 +32,31 @@ class LoginScreen extends ConsumerWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset('assets/icon/freelog_icon.png', width: 100, height: 100),
-              // Icon(Icons.book, size: 64, color: Colors.blueGrey),
+              Image.asset('assets/icon/icon_log.png', width: 90, height: 90),
+              // Icon(Icons.book, size: 70, color: AppColors.primary),
+              SizedBox(height: 2),
               Text('Free Log', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
-              SizedBox(height: 16),
+              SizedBox(height: 30),
 
               LoginButtonWidget(
+                color: Color(0xfff2f2f2),
+                image: 'assets/icon/google_icon.png',
+                text: 'Sign in with Google',
+                isLoading: googleLoading,
+                onPressed: () => ref.read(authNotifierProvider.notifier).signInWithGoogle(),
+              ),
+              SizedBox(height: 8),
+              LoginButtonWidget(
                 color: Color(0xffFEE500),
-                icon: Icons.login,
+                image: 'assets/icon/kakao_icon.png',
                 text: 'Sign in with Kakao',
                 isLoading: kakaoLoading,
                 onPressed: () => ref.read(authNotifierProvider.notifier).signInWithKakao(),
               ),
-              SizedBox(height: 16),
-              LoginButtonWidget(
-                color: Color(0xfff2f2f2),
-                icon: Icons.login,
-                text: 'Sign in with Google',
-                isLoading: googleLoading,
-                onPressed: () => ref.read(authNotifierProvider.notifier).signInWithGoogle(),
+              SizedBox(height: 2),
+              Text(
+                AppLocalizations.of(context)!.privacyPolicy,
+                style: AppTextStyles.caption(context).copyWith(color: Colors.grey),
               ),
             ],
           ),
--- lib/features/auth/presentation/widgets/login_button_widget.dart
+++ lib/features/auth/presentation/widgets/login_button_widget.dart
@@ -3,14 +3,14 @@ import 'package:free_log/core/utils/responsive_utils.dart';
 
 class LoginButtonWidget extends StatelessWidget {
   final Color color;
-  final IconData icon;
+  final String image;
   final String text;
   final VoidCallback? onPressed;
   final bool isLoading;
   const LoginButtonWidget({
     super.key,
     required this.color,
-    required this.icon,
+    required this.image,
     required this.text,
     this.onPressed,
     this.isLoading = false,
@@ -24,24 +24,20 @@ class LoginButtonWidget extends StatelessWidget {
       child: FilledButton(
         style: FilledButton.styleFrom(
           backgroundColor: color,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
         ),
         onPressed: isLoading ? null : onPressed,
         child: isLoading
             ? SizedBox(
                 width: 24,
                 height: 24,
-                child: CircularProgressIndicator(
-                  strokeWidth: 2,
-                  color: Colors.white,
-                ),
+                child: CircularProgressIndicator(strokeWidth: 2, color: Colors.white),
               )
             : Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Icon(icon, color: Colors.black),
-                  SizedBox(width: 8),
+                  Image.asset(image, width: 20, height: 20),
+                  SizedBox(width: 10),
                   Text(text, style: TextStyle(color: Colors.black)),
                 ],
               ),
