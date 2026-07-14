3faed3eee1bcb5b687d95d99ad5940ac4250a9e7
gaeunpark7 <hen77360@gmail.com>
2026-07-14 19:01:24 +0900

    feat: FCM 기반 마감일 푸시 알림 기능 구현

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:free_log/core/router/app_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:free_log/core/services/fcm_service.dart';
 import 'package:free_log/core/theme/app_colors.dart';
+import 'package:free_log/di/auth_provider_setup.dart';
 import 'package:free_log/l10n/app_localizations.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class FrelogApp extends ConsumerWidget {
   const FrelogApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(authStateProvider, (prev, next) {
+      next.whenData((session) async {
+        // 로그인 됐을 때 FCM 토큰 저장
+        final fcmService = FcmService(Supabase.instance.client);
+        await fcmService.initialize();
+      });
+    });
     final router = ref.watch(appRouterProvider);
 
     return MaterialApp.router(
